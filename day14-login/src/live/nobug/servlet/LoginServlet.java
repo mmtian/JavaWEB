e47ad6b348d3ef11de3fe531063b64e04193ea49
mmtian <mm_tian@qq.com>
2020-10-05 20:43:45 +0800

    改用使用BeanUtils封装对象

--- day14-login/src/live/nobug/servlet/LoginServlet.java
+++ day14-login/src/live/nobug/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package live.nobug.servlet;
 
 import live.nobug.dao.UserDao;
 import live.nobug.domain.User;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,18 +10,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
 
-        String username = request.getParameter("username");
+        User loginUser = new User();
+
+        /*String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        User loginUser = new User();
         loginUser.setUsername(username);
-        loginUser.setPassword(password);
+        loginUser.setPassword(password);*/
+
+        try {
+            BeanUtils.populate(loginUser, request.getParameterMap());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
         UserDao userDao = new UserDao();
         User user = userDao.login(loginUser);
