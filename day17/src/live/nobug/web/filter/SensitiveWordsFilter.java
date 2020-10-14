a030b15187ba9166ea75c7e93f8c1e17d7852d0f
mmtian <mm_tian@qq.com>
2020-10-14 19:14:01 +0800

    修复过滤器小bug

--- day17/src/live/nobug/web/filter/SensitiveWordsFilter.java
+++ day17/src/live/nobug/web/filter/SensitiveWordsFilter.java
@@ -2,17 +2,16 @@ package live.nobug.web.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-//@WebFilter("/*")
+@WebFilter("/*")
 public class SensitiveWordsFilter implements Filter {
     List<String> list = new ArrayList<>();
     public void destroy() {
@@ -22,33 +21,66 @@ public class SensitiveWordsFilter implements Filter {
         ServletRequest proxy_req = (ServletRequest) Proxy.newProxyInstance(req.getClass().getClassLoader(), req.getClass().getInterfaces(), new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                // 增强getParameter 方法
                 if(method.getName().equals("getParameter")){
                     String value = (String) method.invoke(req, args);
-                    if(value != null && !value.equals("")){
-                        for (String s : list) {
-                            if(value.contains(s)){
-                                value = value.replaceAll(s, "***");
+                    return replace(value);
+                }
+
+                // 增强getParameter 方法
+                if(method.getName().equals("getParameters")){
+                    String[] values = (String[]) method.invoke(req, args);
+                    if(values != null){
+                        for (int i = 0; i < values.length; i++) {
+                            values[i] = replace(values[i]);
+                        }
+                    }
+                    return values;
+                }
+
+                // 增强getParameterMap方法
+                if(method.getName().equals("getParameterMap")){
+                    Map<String, String[]> map = (Map<String, String[]>) method.invoke(req, args);
+                    Set<String> keySet = map.keySet();
+                    for (String key : keySet) {
+                        String[] values = map.get(key);
+                        if(values != null){
+                            for (int i = 0; i < values.length; i++) {
+                                values[i] = replace(values[i]);
                             }
                         }
                     }
-                    return value;
+                    return map;
                 }
-                String value = (String) method.invoke(req, args);
-                return value;
+
+                return method.invoke(req, args);
             }
         });
 
         chain.doFilter(proxy_req, resp);
     }
 
+    private String replace(String value){
+        if(value != null && !value.equals("")){
+            for (String s : list) {
+                if(value.contains(s)){
+                    value = value.replaceAll(s, "***");
+                }
+            }
+        }
+        return value;
+    }
+
     public void init(FilterConfig config) throws ServletException {
         String realPath = config.getServletContext().getRealPath("/WEB-INF/classes/sensitive_words.txt");
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(realPath));
+            InputStreamReader isr = new InputStreamReader(new FileInputStream(realPath),"utf-8");
+            BufferedReader reader = new BufferedReader(isr);
             String line;
             while ((line = reader.readLine())!=null){
                 list.add(line);
             }
+            System.out.println(list);
             reader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
